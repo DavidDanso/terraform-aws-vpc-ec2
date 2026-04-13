d648db5e011c4bf97a883f31e8534569693b1b94
DavidDanso <desiingerboy@icloud.com>
2026-04-13 02:48:50 +0000

    refactor: move terraform and provider configuration to dedicated provider.tf file

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 module "networking" {
   source               = "./modules/networking"
   vpc_cidr             = var.vpc_cidr
